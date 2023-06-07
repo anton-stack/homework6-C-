c845f9e94209a194666ec40c3baf6181ce95aecc
anton <antonkovalev2019s@gmail.com>
2023-06-07 19:32:29 +0300

    1

---
+++ C#/Program.cs
@@ -0,0 +1,46 @@
+﻿//  Задача 1
+
+// Console.WriteLine("Введите количество чисел в массиве");
+// int i = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите начальное число массива ");
+// int s = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите последнее число массива ");
+// int e = Convert.ToInt32(Console.ReadLine());
+// int count = 0;
+// int[] numbers = new int[i];
+// Random rnd = new Random();
+// System.Console.WriteLine("Числа массива: ");
+// for (byte j = 0; j < numbers.Length; j++)
+//     {
+//         numbers[j] = rnd.Next(s, e);
+//         System.Console.WriteLine(numbers[j]);
+
+//         if (numbers[j] > 0)
+//         {
+//             count++;
+//         }
+
+//     }
+
+// System.Console.WriteLine("Количество чисел больше нуля в массиве => "  + count);
+
+// Задача 2
+
+// Console.Write("Введите k1: ");
+// var k1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите b1: ");
+// var b1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите k2: ");
+// var k2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите b2: ");
+// var b2 = Convert.ToDouble(Console.ReadLine());
+ 
+ 
+// var x = -(b1 - b2) / (k1 - k2);
+// var y = k1 * x + b1;
+ 
+// x = Math.Round(x, 3);
+// y = Math.Round(y, 3);
+ 
+// Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
